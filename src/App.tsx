d2301bb498496545d02f2b299cfe5220800e6537
berk.ozzambak <berkozzambak@gmail.com>
2024-02-01 00:38:29 +0300

    feat: add basic routes with protected route component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import AuthView from "./Views/Auth";
+import Dashboard from "./Views/Dashboard";
+
+const ProtectedRoute = () => {
+  const user = "";
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }
+  return <Outlet />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AuthView />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/*" element={<AuthView />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
