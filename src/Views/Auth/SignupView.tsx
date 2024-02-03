b48a5ccfa5aad7cba57bb8b4b808098a6de1cba8
berk.ozzambak <berkozzambak@gmail.com>
2024-02-03 11:40:33 +0300

    feat: creeate signup view and form

--- src/Views/Auth/SignupView.tsx
+++ src/Views/Auth/SignupView.tsx
@@ -1,11 +1,17 @@
+import { Link } from '@mui/material'
 import SignupForm from '../../Components/Auth/SignupForm'
 import './auth.css'
 
 function SignupView() {
   return (
     <div className="auth-view-wrapper">
-        <SignupForm/>
+    <h3>Quiz Generator</h3>
+    <SignupForm />
+    <div className="auth-form-link-container">
+      <span>Already have an account?</span>
+      <Link href="/signup">Login</Link>
     </div>
+  </div>
   )
 }
 
