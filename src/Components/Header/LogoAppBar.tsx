e0dc51a9f8e4f29f8698cc82143ca2185649e8e7
berk.ozzambak <berkozzambak@gmail.com>
2024-02-12 01:07:56 +0100

    fix: replace navbar with mui appbar

---
+++ src/Components/Header/LogoAppBar.tsx
@@ -0,0 +1,191 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { Link } from "@mui/material";
+import { BlurOn } from "@mui/icons-material";
+import { SignOut } from "../../Api/auth";
+
+const pages = ["Dashboard", "Quizzes"];
+const settings = ["Logout"];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static" color="primary">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <BlurOn sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+              alignContent: "center",
+            }}
+          >
+            Qgen
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem>
+                <Link href="/dashboard" underline="none">
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "black", display: "block" }}
+                  >
+                    Dashboard
+                  </Button>
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href={`/quizzes`} underline="none">
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "black", display: "block" }}
+                  >
+                    Quizzes
+                  </Button>
+                </Link>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <BlurOn sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Qgen
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Link href="/dashboard" underline="hover">
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Dashboard
+              </Button>
+            </Link>
+            <Link href={`/quizzes`} underline="hover">
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Quizzes
+              </Button>
+            </Link>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar
+                  alt="Username"
+                  sx={{ background: "#eeeeee", color: "#1876d2" }}
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem onClick={SignOut}>
+                <Typography textAlign="center">Logout</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,33 +3,14 @@ import Flex from "../../Ui/Flex";
 import NavbarLink from "./NavbarLink";
 import { Button } from "@mui/material";
 import { SignOut } from "../../Api/auth";
+import ResponsiveAppBar from "./LogoAppBar";
 
 function Header() {
   return (
-    <Flex
-      dir="row"
-      className="header-container flex-items-center flex-justify-space-between"
-    >
-      <Flex dir="row" className="header-left-section flex-items-center">
-        <HeaderNavbar />
-      </Flex>
-
-      <div className="signout-section">
-        <Button variant="outlined" size="small" onClick={SignOut}>
-          Sign Out
-        </Button>
-      </div>
+    <Flex dir="row" className="header-container">
+      <ResponsiveAppBar></ResponsiveAppBar>
     </Flex>
   );
 }
 
 export default Header;
-
-const HeaderNavbar = () => {
-  return (
-    <Flex dir="row" className="navbar-links-container">
-      <NavbarLink to="/dashboard">Dashboard</NavbarLink>
-      <NavbarLink to="/quizzes">Quizzes</NavbarLink>
-    </Flex>
-  );
-};
