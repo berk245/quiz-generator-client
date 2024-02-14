30de7a90003f478ad5d3e163d20621a1f7392b2c
berk.ozzambak <berkozzambak@gmail.com>
2024-02-14 23:44:43 +0100

    feat: add query quiz functions

---
+++ src/Api/quizzes.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import Cookies from "js-cookie";
+
+export const GetQuizzesFn = async () => {
+  const res = await axios.get(process.env.REACT_APP_SERVER_URL + "/quiz", {
+    headers: {
+      Authorization: Cookies.get("auth_token"),
+    },
+  });
+  return res.data || [];
+};
+
+export const useGetQuizzes = () => {
+  return useQuery({
+    queryKey: ["get_quizzes"],
+    queryFn: GetQuizzesFn,
+  });
+  //
+};
