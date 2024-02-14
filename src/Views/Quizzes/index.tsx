3142c2d2c521fffad9840af0be98e92c80d0eb39
berk.ozzambak <berkozzambak@gmail.com>
2024-02-14 23:47:37 +0100

    feat: implement quizzes view

--- src/Views/Quizzes/index.tsx
+++ src/Views/Quizzes/index.tsx
@@ -1,4 +1,4 @@
-import { Button, SelectChangeEvent } from "@mui/material";
+import { Button, LinearProgress, SelectChangeEvent } from "@mui/material";
 import DefaultLayout from "../../Layouts/DefaultLayout";
 import Flex from "../../Ui/Flex";
 import "./quizzes-view.css";
@@ -8,30 +8,20 @@ import SearchInput from "../../Ui/SearchInput";
 import { SortBySelector } from "../../Components/Quizzes/SortBySelector";
 import SingleQuizBox from "../../Components/Quizzes/SingleQuizBox";
 import { QuizType } from "../../types";
-const fakeQuizzes = [
-  {
-    id: "123456",
-    quiz_title: "Clean Code Quiz 3",
-    quiz_description: "Module description based set of questions",
-    created_at: new Date(),
-    is_active: true,
-  },
-  {
-    id: "1234",
-    quiz_title: "Collab test",
-    quiz_description: "Some trial and error experiment",
-    created_at: new Date(),
-    is_active: true,
-  },
-];
+import { useGetQuizzes } from "../../Api/quizzes";
+import { searchFilter, sortFilter } from "../../Components/Quizzes/helpers";
 
 function QuizzesView() {
   const [sortBy, setSortBy] = useState("Date (ascending)");
   const [searchTerm, setSearchTerm] = useState("");
 
-  const filteredQuizzes = fakeQuizzes
-    .filter(searchFilter(searchTerm))
-    .sort(sortFilter(sortBy));
+  const { data: serverResponse, isLoading, isError, error } = useGetQuizzes();
+
+  const filteredQuizzes: QuizType[] = serverResponse?.data
+    ? serverResponse.data
+        .filter(searchFilter(searchTerm))
+        .sort(sortFilter(sortBy))
+    : [];
 
   const handleSortChange = (e: SelectChangeEvent) => {
     setSortBy(e.target.value);
@@ -66,60 +56,33 @@ function QuizzesView() {
             blanditiis earum velit quo totam.
           </span>
         </Flex>
-        <Flex className="quizzes-view-content-section" dir="column">
-          <Flex className="search-sort-line flex-items-center" dir="row">
-            <SearchInput
-              labelText="Search Quizzes"
-              onValueChange={handleSearch}
-              variant="outlined"
-              size="small"
-              className="quiz-search-input"
-            ></SearchInput>
-            <SortBySelector
-              sortValue={sortBy}
-              handleSortChange={handleSortChange}
-            />
+        {isLoading ? (
+          <LinearProgress />
+        ) : (
+          <Flex className="quizzes-view-content-section" dir="column">
+            <Flex className="search-sort-line flex-items-center" dir="row">
+              <SearchInput
+                labelText="Search Quizzes"
+                onValueChange={handleSearch}
+                variant="outlined"
+                size="small"
+                className="quiz-search-input"
+              ></SearchInput>
+              <SortBySelector
+                sortValue={sortBy}
+                handleSortChange={handleSortChange}
+              />
+            </Flex>
+            <div className="quiz-boxes-container" dir="row">
+              {filteredQuizzes.map((quiz) => {
+                return <SingleQuizBox key={quiz.quiz_id} quiz={quiz} />;
+              })}
+            </div>
           </Flex>
-
-          <div className="quiz-boxes-container" dir="row">
-            {filteredQuizzes.map((quiz) => {
-              return <SingleQuizBox key={quiz.id} quiz={quiz} />;
-            })}
-          </div>
-        </Flex>
+        )}
       </Flex>
     </DefaultLayout>
   );
 }
 
 export default QuizzesView;
-
-// Search filter function
-const searchFilter = (term: string) => (quiz: QuizType) => {
-  return (
-    quiz.quiz_title.toLowerCase().includes(term.toLowerCase()) ||
-    quiz.quiz_description.toLowerCase().includes(term.toLowerCase())
-  );
-};
-
-// Sort filter function
-const sortFilter = (sortBy: string) => (quizA: QuizType, quizB: QuizType) => {
-  console.log(sortBy);
-  if (sortBy === "Date (ascending)") {
-    return (
-      new Date(quizA.created_at).getTime() -
-      new Date(quizB.created_at).getTime()
-    );
-  } else if (sortBy === "Date (descending)") {
-    return (
-      new Date(quizB.created_at).getTime() -
-      new Date(quizA.created_at).getTime()
-    );
-  } else if (sortBy === "Name (a-z)") {
-    return quizA.quiz_title.localeCompare(quizB.quiz_title);
-  } else if (sortBy === "Name (z-a)") {
-    return quizB.quiz_title.localeCompare(quizA.quiz_title);
-  }
-  // Add more conditions for other sorting options if needed
-  return 0;
-};
