d3e64c49c73d6408160dbd0351d85a9b015e2f60
berk.ozzambak <berkozzambak@gmail.com>
2024-03-20 11:56:31 +0100

    feat: add intermediary steps during quiz creation

--- src/Components/CreateQuiz/Backdrops/LoadingBackdrop.tsx
+++ src/Components/CreateQuiz/Backdrops/LoadingBackdrop.tsx
@@ -1,6 +1,32 @@
 import { Backdrop, CircularProgress } from "@mui/material";
+import { useEffect, useState } from "react";
+
+const quizCreationSteps = [
+  "Setting things up",
+  "Transferring data",
+  "Compiling quiz information",
+  "Analyzing teaching material",
+  "Adding keywords and quiz instructions",
+  "Creating quiz",
+];
 
 export const LoadingBackdrop = ({ isPending }: { isPending: boolean }) => {
+  const [loadingText, setLoadingText] = useState(quizCreationSteps[0]);
+
+  useEffect(() => {
+    let counter = 1;
+    const interval = setInterval(() => {
+      if (counter === quizCreationSteps.length) {
+        clearInterval(interval);
+        return;
+      }
+      setLoadingText(quizCreationSteps[counter]);
+      counter++;
+    }, 3500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Backdrop
       sx={{
@@ -12,7 +38,7 @@ export const LoadingBackdrop = ({ isPending }: { isPending: boolean }) => {
     >
       <CircularProgress color="inherit" />
       <br />
-      <span>Creating your quiz</span>
+      <span>{loadingText}</span>
     </Backdrop>
   );
 };
