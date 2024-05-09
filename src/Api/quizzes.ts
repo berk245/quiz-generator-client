5f1d69691e02a8c99e05fa01aa7a819c0a9d5a78
berk.ozzambak <berkozzambak@gmail.com>
2024-05-10 00:22:40 +0200

    feat: add learning objectives in the quiz creation request

--- src/Api/quizzes.ts
+++ src/Api/quizzes.ts
@@ -43,7 +43,7 @@ const CreateQuizFn = async (props: CreateQuizRequestType) => {
   formData.append("quiz_description", props.description);
   formData.append("keywords", props.keywords.join(","));
   formData.append("source_file", props.files[0]);
-  formData.append("meta_prompt", props.learningObjectives);
+  formData.append("learning_objectives", props.learningObjectives);
 
   const res = await axios({
     method: "POST",
